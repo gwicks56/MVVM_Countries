f7b3dad44e56bcb9e00a1c0a91c9cb306e3c0d52
gwicks56 <geordiewicks@gmail.com>
2022-07-14 11:33:07 +1000

    Added some unit tests

--- app/src/test/java/com/gwicks/countries/ListViewModelTest.kt
+++ app/src/test/java/com/gwicks/countries/ListViewModelTest.kt
@@ -1,16 +1,28 @@
 package com.gwicks.countries
 
+import android.widget.ListView
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.gwicks.countries.model.CountriesService
+import com.gwicks.countries.model.Country
+import com.gwicks.countries.viewmodel.ListViewModel
 import io.reactivex.Scheduler
+import io.reactivex.Single
 import io.reactivex.android.plugins.RxAndroidPlugins
 import io.reactivex.disposables.Disposable
 import io.reactivex.internal.schedulers.ExecutorScheduler
 import io.reactivex.plugins.RxJavaPlugins
+import junit.framework.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
 import java.time.Instant
 import java.util.concurrent.Executor
 import java.util.concurrent.TimeUnit
+import javax.inject.Inject
 
 class ListViewModelTest {
 
@@ -18,6 +30,46 @@ class ListViewModelTest {
     var rule = InstantTaskExecutorRule()
 
 
+    @Mock
+    lateinit var countriesService: CountriesService
+
+    @InjectMocks
+    var listViewModel = ListViewModel()
+
+    private var testSingle: Single<List<Country>>? = null
+
+
+    @Before
+    fun setup(){
+        MockitoAnnotations.initMocks(this)
+    }
+
+    @Test
+    fun getCountriesSucess(){
+        val country = Country("countryName", "capital", "url")
+        val countriesList: ArrayList<Country> = arrayListOf(country)
+
+        testSingle = Single.just(countriesList)
+
+        Mockito.`when`(countriesService.getCountries()).thenReturn(testSingle)
+
+        listViewModel.refresh()
+
+        assertEquals(1, listViewModel.countries.value?.size)
+        assertEquals(false, listViewModel.countryLoadError.value)
+        assertEquals(false, listViewModel.loading.value)
+
+    }
+
+    @Test
+    fun getCountriesFail(){
+        testSingle = Single.error(Throwable())
+        Mockito.`when`(countriesService.getCountries()).thenReturn(testSingle)
+        listViewModel.refresh()
+        assertEquals(true, listViewModel.countryLoadError.value)
+        assertEquals(false, listViewModel.loading.value)
+
+    }
 
     @Before
     fun setUpRxSchedulers(){
